32a31f300322861d11d0537dbf398030093c927e
karolbon <karolbon@stud.ntnu.no>
2020-05-21 07:30:33 +0200

    Refactor models

--- models.py
+++ models.py
@@ -6,64 +6,33 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
-from preprocessing.read_data import load_dataframe, save_dataframe
-
-
-def use_SVC(data):
-    y = df['Label']
-    X = df.drop(columns=['Label', 'User_ID', 'Tweets'])
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.50, random_state=42)
-
-    clf = make_pipeline(StandardScaler(), SVC())
-
-    clf.fit(X_train, y_train)
-
-    predicted = clf.predict(X_test)
-
-    acc = accuracy_score(predicted, y_test)
-
-    print("Accuracy for SVM: ", acc)
+from sklearn.ensemble import VotingClassifier
 
 
-def use_naive_bayes(data):
-    y = df['Label']
-    X = df.drop(columns=['Label', 'User_ID', 'Tweets'])
+class SVMClassifier():
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.10, random_state=42)
-
-    clf = MultinomialNB()
-    clf.fit(X_train, y_train)
-
-    predicted = clf.predict(X_test)
-
-    acc = accuracy_score(predicted, y_test)
-
-    print("Accuracy for Naive Bayes: ", acc)
+    def __init__(self):
+        self.classifier = make_pipeline(StandardScaler(), SVC())
 
 
-def use_random_forest(data):
-    y = df['Label']
-    X = df.drop(columns=['Label', 'User_ID', 'Tweets'])
+class NaiveBayesClassifier():
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.10, random_state=42)
-
-    clf = RandomForestClassifier()
-    clf.fit(X_train, y_train)
-
-    predicted = clf.predict(X_test)
-
-    acc = accuracy_score(predicted, y_test)
-
-    print("Accuracy for Random Forest: ", acc)
+    def __init__(self):
+        self.classifier = MultinomialNB()
 
 
-df = load_dataframe('training_df_with_basic_features')
+class RandomForestWrapperClassifier():
 
-use_SVC(df)
-use_naive_bayes(df)
-use_random_forest(df)
+    def __init__(self):
+        self.classifier = RandomForestClassifier()
+
+
+class EnsembleClassifier():
+
+    def __init__(self):
+        clf1 = make_pipeline(StandardScaler(), SVC())
+        clf2 = MultinomialNB()
+        clf3 = RandomForestClassifier()
+
+        self.voting_classifier = VotingClassifier(
+            estimators=[('svm', clf1), ('nb', clf2), ('rf', clf3)], voting='hard')
