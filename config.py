81bd3e221d29c9dd01de748f431a06a51b7c6ad8
karolbon <karolbon@stud.ntnu.no>
2020-05-21 07:32:01 +0200

    Add main og config file for setup of experiments

---
+++ config.py
@@ -0,0 +1,9 @@
+
+config = {
+    'read_and_save_raw_data_as_dataframe': False,
+    'path_to_root_training_data': 'data/training/',
+    'filename_existing_data_df': 'training_df',
+
+    'load_preprocessed_dataframe': True,
+    'filename_exisiting_preprocessed_df': 'training_df_preprocessed_features'
+}
